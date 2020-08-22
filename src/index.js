4bbfe6890eadef87b479bf1412ec1392e42c1557
Juan Mireles <jmireles12@gmail.com>
2020-08-22 16:31:51 -0500

    completed right

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import store from './store'
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
