d1fee9b5d4fadffc5841b5f90a07671868bd2935
Juan Mireles <jmireles12@gmail.com>
2020-08-29 16:21:02 -0500

    refactored Trelloyes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import store from './store'
-import App from './App';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
 
-ReactDOM.render(<App store={store} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
